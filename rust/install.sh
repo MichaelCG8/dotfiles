4422dfa740574d0fad17dd5a630e757a351a9a9b
Michael Garbutt <michael.c.garbutt@gmail.com>
2021-05-06 22:45:45 +0100

    Added rust install and tealdeer

---
+++ rust/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+######################
+# The rust toolchain #
+######################
+
+# https://www.rust-lang.org/tools/install
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+###################
+# Useful binaries #
+###################
+
+# Rust implementation of tldr
+# https://github.com/tldr-pages/tldr
+# https://crates.io/crates/tealdeer
+cargo install tealdeer
+
