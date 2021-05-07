7c8b972ff5746e63b30512857571bdd83be1eda1
Michael Garbutt <michael.garbutt@pommelhorse.com>
2021-05-07 12:18:06 +0100

    Adding alias for tldr usage

---
+++ rust/aliases.zsh
@@ -0,0 +1,7 @@
+#######################################
+# Aliases for installed rust binaries #
+#######################################
+
+# Use tldr in pager mode, rather than printing to stdout.
+alias tldr="tldr -p"
+
