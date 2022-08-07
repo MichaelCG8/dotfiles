c2a7cf0782ccf09b722f4d4edac789f2de99576d
Michael Garbutt <michael.c.garbutt@gmail.com>
2022-08-07 11:05:08 +0100

    Improving applications install script

--- applications/install.sh
+++ applications/install.sh
@@ -1,221 +1,163 @@
 #!/usr/bin/env bash
 
+FedoraApps=(
+	"g++ installName=gcc-c++"
+	"thunderbird"
+	"shellcheck installName=ShellCheck"
+)
+LinuxApps=(
+	"steam askConfirm"
+	"inkscape"
+	"vlc"
+	"gimp"
+)
+MacApps=(
+	"inkscape installOpt=--cask"
+	"vlc installOpt=--cask"
+	"gimp installOpt=--cask"
+	"shellcheck"
+)
+
+
+# FIXME: installCommand can be the only option. E.g. askConfirm would be ignored if later.
+AllOSApps=(
+	"ffmpeg"
+	"cmake"
+	"fzf installCommand=\"git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install --key-bindings --completion --no-update-rc --no-zsh --no-bash\""
+)
+
 echo
 echo "Running applications install script"
+echo
 
-which ffmpeg > /dev/null
-if [[ $? == 0 ]]
-then
-    echo "ffmpeg already installed"
-else
-    unameOut="$(uname -s)"
-    if [[ $unameOut == "Linux" ]]
-    then
-        distroName="$(grep "^NAME=" /etc/os-release)"
-        if [[ "$distroName" == *"Fedora"* ]]
-        then
-            sudo dnf install ffmpeg 
-        elif [[ "$distroName" == *"Mint"* ]]
-        then
-            echo "ffmpeg install not automated on Mint."
-        else
-            echo "Unknown Linux distribution: $distroName"
-            exit 1
-        fi
-    elif [[ $unameOut == "Darwin" ]]
-    then
-        echo "ffmpeg install not automated on Mac."
-    else
-        echo "Unknown kernel: $unameOut"
-        exit 1
-    fi
-fi
-
-which fzf > /dev/null
-if [[ $? == 0 ]]
-then
-    echo "fzf already installed"
-else
-    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-    ~/.fzf/install --key-bindings --completion --no-update-rc --no-zsh --no-bash
-fi
-
-which vlc > /dev/null
-if [[ $? == 0 ]]
-then
-    echo "vlc already installed"
-else
-    unameOut="$(uname -s)"
-    if [[ $unameOut == "Linux" ]]
-    then
-        distroName="$(grep "^NAME=" /etc/os-release)"
-        if [[ "$distroName" == *"Fedora"* ]]
-        then
-            sudo dnf install vlc 
-        elif [[ "$distroName" == *"Mint"* ]]
-        then
-            echo "vlc install not automated on Mint."
-        else
-            echo "Unknown Linux distribution: $distroName"
-            exit 1
-        fi
-    elif [[ $unameOut == "Darwin" ]]
-    then
-        echo "vlc install not automated on Mac."
-    else
-        echo "Unknown kernel: $unameOut"
-        exit 1
-    fi
-fi
+kernel="$(uname -s)"
+case $kernel in
+	"Linux")
+		distro="$(grep "^NAME=" /etc/os-release)"
+		case $distro in
+			*"Fedora"*)
+				installer="dnf"
+				;;
+			*"Mint"* | *"Ubuntu"*)
+				installer="apt-get"
+				;;
+			*)
+				>&2 echo "Unrecognised distro: $distro"
+				exit 1
+				;;
+		esac
+		;;
+	"Darwin")
+		installer="brew"
+		;;
+	*)
+		>&2 echo "Unrecognised kernel: $kernel"
+		exit 1
+		;;
+esac
 
 
-which steam > /dev/null
-if [[ $? == 0 ]]
-then
-    echo "Steam already installed"
-else
-    read -n1 -p "Do you want to install Steam for gaming? [y,n]" doit 
-    case $doit in  
-      y|Y)
-        unameOut="$(uname -s)"
-        if [[ $unameOut == "Linux" ]]
-        then
-            distroName="$(grep "^NAME=" /etc/os-release)"
-            if [[ "$distroName" == *"Fedora"* ]]
-            then
-                sudo dnf install steam
-            elif [[ "$distroName" == *"Mint"* ]]
-            then
-                sudo apt-get install steam
-            else
-                echo "Unknown Linux distribution: $distroName"
-                exit 1
-            fi
-        elif [[ $unameOut == "Darwin" ]]
-        then
-            echo "Steam install unsupported on Mac"
-        else
-            echo "Unknown kernel: $unameOut"
-            exit 1
-        fi	
-	;; 
-      *) echo Skipping Steam install ;; 
-    esac
-fi
+needs_installed () {
+	appRunName=$1
+	if which "$appRunName" >> /dev/null
+	then
+		echo "$appRunName is already installed"
+		return 1
+	fi
+	return 0
+}
 
 
-which inkscape > /dev/null
-if [[ $? == 0 ]]
-then
-    echo "Inkscape already installed"
-else
-    unameOut="$(uname -s)"
-    if [[ $unameOut == "Linux" ]]
-    then
-        distroName="$(grep "^NAME=" /etc/os-release)"
-        if [[ "$distroName" == *"Fedora"* ]]
-        then
-            sudo dnf install inkscape
-        elif [[ "$distroName" == *"Mint"* ]]
-        then
-	    echo "Inkscape install not automated on Mint"
-        else
-            echo "Unknown Linux distribution: $distroName"
-            exit 1
-        fi
-    elif [[ $unameOut == "Darwin" ]]
-    then
-        brew install --cask inkscape
-    else
-        echo "Unknown kernel: $unameOut"
-        exit 1
-    fi	
-fi
+parse_app () {
+	appArray=($@)
+	runName=${appArray[0]}
+	elements=("${appArray[@]:1}")
+	installName="$runName"
+	installOptionsArr=()
+	if [[ ${elements[*]} == "installCommand="* ]]
+	then
+		echo "$runName" installComand="$installCommand"
+	else
+		for el in "${elements[@]}"
+		do
+			case $el in
+				"installName="*)
+					installName=${el:12}
+					;;
+				"askConfirm")
+					askConfirm="confirm"
+					;;
+				"installOpt="*)
+					installOptionsArr+=("${el:11}")
+					;;
+				*)
+					>&2 echo "Bad element \"$el\" in (${appArray[*]})"
+					exit 1
+					;;
+			esac
+		done
+		echo "$runName" "$installName" "$askConfirm" \""${installOptionsArr[*]}"\"
+	fi
+}
 
 
-which thunderbird > /dev/null
-if [[ $? == 0 ]]
-then
-    echo "Thunderbird already installed"
-else
-    unameOut="$(uname -s)"
-    if [[ $unameOut == "Linux" ]]
-    then
-        distroName="$(grep "^NAME=" /etc/os-release)"
-        if [[ "$distroName" == *"Fedora"* ]]
-        then
-            sudo dnf install thunderbird
-        elif [[ "$distroName" == *"Mint"* ]]
-        then
-	    echo "Thunderbird install not automated on Mint"
-        else
-            echo "Unknown Linux distribution: $distroName"
-            exit 1
-        fi
-    elif [[ $unameOut == "Darwin" ]]
-    then
-        echo "Thunderbird install not automated on Mac"
-    else
-        echo "Unknown kernel: $unameOut"
-        exit 1
-    fi	
-fi
+install_list () {
+	appList=("$@")
+	for app in "${appList[@]}"
+	do
+		appArray=("$app")
+		appArrayStr=$(parse_app "${appArray[@]}")
+		read -ra appArray <<< $appArrayStr
+		appRunName="${appArray[0]}"
+		if needs_installed "$appRunName"
+		then
+			if [[ ${appArray[1]} == "installCommand="* ]]
+			then
+				installCommand="${appArray[2]}"
+				installCommand="${installCommand:15}"
+				$installCommand
+			else
+				appInstallName="${appArray[1]}"
+
+				if [[ "${appArray[2]}" == "confirm" ]]
+				then
+					confirm="${appArray[2]}"
+					installOptions="${appArray[3]}"
+				else
+					installOptions="${appArray[2]}"
+				fi
 
 
-which gimp > /dev/null
-if [[ $? == 0 ]]
-then
-    echo "GIMP already installed"
-else
-    unameOut="$(uname -s)"
-    if [[ $unameOut == "Linux" ]]
-    then
-        distroName="$(grep "^NAME=" /etc/os-release)"
-        if [[ "$distroName" == *"Fedora"* ]]
-        then
-            sudo dnf install gimp
-        elif [[ "$distroName" == *"Mint"* ]]
-        then
-	    echo "GIMP install not automated on Mint"
-        else
-            echo "Unknown Linux distribution: $distroName"
-            exit 1
-        fi
-    elif [[ $unameOut == "Darwin" ]]
-    then
-        echo "GIMP install not automated on Mac"
-    else
-        echo "Unknown kernel: $unameOut"
-        exit 1
-    fi	
-fi
+				if [ -n "${confirm+x}" ]
+				then
+					read -n1 -p "Do you want to install ${appRunName}? y/[n]" doit 
+					case $doit in  
+						y|Y)
+							sudo $installer install "$installOptions" "$appInstallName"
+							;;
+						*)
+							echo Skipping installing "$appRunName"
+							;;
+					esac
+				else
+					sudo $installer install "$installOptions" "$appInstallName"
+				fi
+			fi
+		fi
+	done
+}
 
-
-which cmake > /dev/null
-if [[ $? == 0 ]]
-then
-    echo "cmake already installed"
-else
-    unameOut="$(uname -s)"
-    if [[ $unameOut == "Linux" ]]
-    then
-        distroName="$(grep "^NAME=" /etc/os-release)"
-        if [[ "$distroName" == *"Fedora"* ]]
-        then
-            sudo dnf install cmake
-        elif [[ "$distroName" == *"Mint"* ]]
-        then
-	    echo "cmake install not automated on Mint"
-        else
-            echo "Unknown Linux distribution: $distroName"
-            exit 1
-        fi
-    elif [[ $unameOut == "Darwin" ]]
-    then
-        echo "cmake install not automated on Mac"
-    else
-        echo "Unknown kernel: $unameOut"
-        exit 1
-    fi	
-fi
+case $kernel in
+	"Linux")
+		install_list "${LinuxApps[@]}"
+		if [[ $distro == *"Fedora"* ]]
+		then
+			install_list "${FedoraApps[@]}"
+		fi
+		;;
+	"Darwin")
+		install_list "${MacApps[@]}"
+		;;
+esac
+install_list "${AllOSApps[@]}"
 
