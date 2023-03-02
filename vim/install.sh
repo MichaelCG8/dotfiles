a14c698308b02d8ed13fbdcb954d29ed19634ead
Michael Garbutt <michael.garbutt@pommelhorse.com>
2023-03-02 14:34:17 +0000

    Adding colorscheme to vim config.

--- vim/install.sh
+++ vim/install.sh
@@ -37,3 +37,15 @@ fi
 curl -fo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+
+success () {
+  printf "\r\033[2K  [ \033[00;32mOK\033[0m ] $1\n"
+}
+
+# Install colorscheme.
+colorscheme_dir="$HOME/.vim/colors"
+mkdir -p "$colorscheme_dir"
+src="$ZSH/vim/my_colorscheme.vim.symlink"
+dst="$colorscheme_dir/my_colorscheme.vim"
+ln -sf "$src" "$dst"
+success "Installed vim colorscheme 'my_colorscheme' to $dst"
