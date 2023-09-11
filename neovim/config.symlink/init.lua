ebbd08e275889a098a5605531eb63998b5b9092c
Michael Garbutt <michael.c.garbutt@gmail.com>
2023-09-11 08:40:00 +0100

    Making neovim telescope search ignore soem file patterns.

--- neovim/config.symlink/init.lua
+++ neovim/config.symlink/init.lua
@@ -275,6 +275,10 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- See `:help telescope` and `:help telescope.setup()`
 require('telescope').setup {
   defaults = {
+    file_ignore_patterns = {
+      "%.pyc",
+      "%~",
+    },
     mappings = {
       i = {
         ['<C-u>'] = false,
