53698b452d805fe19cf627482f3d05d49d4282fe
Michael Garbutt <michael.c.garbutt@gmail.com>
2024-02-24 16:03:49 +0000

    Adding tmux configuration.

---
+++ tmux/install.sh
@@ -0,0 +1,4 @@
+# These commmands were run to get .tmux.conf the first time, but now they are version controlled.
+# tmux.conf.local is the one I change. I'm not meant to touch tmux.conf.
+# wget --output-document tmux.conf.symlink https://raw.githubusercontent.com/gpakosz/.tmux/master/.tmux.conf
+# wget --output-document tmux.conf.local.symlink https://raw.githubusercontent.com/gpakosz/.tmux/master/.tmux.conf.local
