b9928604ab356daba6c063efa75782692c6bb5ea
Michael Garbutt <michael.c.garbutt@gmail.com>
2024-02-27 02:05:45 +0000

    Simplifying aliases.

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,38 +1,8 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-# if $(gls &>/dev/null)
-# then
-#   alias ls="gls -F --color"
-#   alias l="gls -lAh --color"
-#   alias ll="gls -l --color"
-#   alias la='gls -A --color'
-# fi
-
-
-# Copied from .bashrc
-################################################################
-
-# enable color support of ls and also add handy aliases
-if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
-
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
-fi
-
 # colored GCC warnings and errors
 #export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
 
 # some more ls aliases
-alias ls='ls -A'
-alias ll='ls -alF'
-alias la='ls -A'
-alias l='ls -CF'
-alias lg='ls -a | grep'
+alias ls='ls --color=auto -A'
+alias ll='ls --color=auto -AlFh'
 
 alias cat='bat'
