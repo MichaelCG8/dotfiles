325b918891572415d90a1039a7b56223deeaf08e
Michael Garbutt <michael.garbutt@pommelhorse.com>
2024-10-22 15:45:57 +0200

    Removing extra argument to tldr, which is now in a config file.

--- rust/aliases.zsh
+++ rust/aliases.zsh
@@ -2,9 +2,6 @@
 # Aliases for installed rust binaries #
 #######################################
 
-# Use tldr in pager mode, rather than printing to stdout.
-alias tldr="tldr --pager"
-
 # Substitute bat for cat.
 alias cat="bat"
 
